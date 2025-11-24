035a51b380acd8eeaf3ae2b216707f7dd214ef21
tg5g25 <tg5g25@soton.ac.uk>
2025-11-24 09:36:29 +0000

    added argument checkt to function add

--- add.py
+++ add.py
@@ -1,3 +1,5 @@
 def add(x: int, y: int) -> int:
+    if not isinstance(x,int) or not isinstance(y,int):
+     raise TypeError("Both arguments must be integers")
     """Returns the sum of two integers."""
     return x + y
